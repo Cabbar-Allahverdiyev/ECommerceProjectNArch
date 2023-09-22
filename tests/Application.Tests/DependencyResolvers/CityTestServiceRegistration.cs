5b75e4e0c1ce2e31943cdaf635185180aa82b535
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-09-23 00:10:23 +0400

    dependency resolvers added for City Tests

---
+++ tests/Application.Tests/DependencyResolvers/CityTestServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Application.Features.Cities.Commands.Create;
+using Application.Tests.Mocks.FakeData;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Tests.DependencyResolvers;
+public static class CityTestServiceRegistration
+{
+    public static void AddCityServices(this IServiceCollection services)
+    {
+        services.AddTransient<CityFakeData>();
+        services.AddTransient<CreateCityCommand>();
+
+        services.AddTransient<CreateCityCommandValidator>();
+    }
+}
