51516631132c0ec586b1dd651de768570095c2c2
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-09-28 23:18:02 +0400

    country create command created

---
+++ src/demoProject/Application/Features/Countries/Commands/Create/CreateCountryCommandValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+
+namespace Application.Features.Countries.Commands.Create;
+
+public class CreateCountryCommandValidator : AbstractValidator<CreateCountryCommand>
+{
+    public CreateCountryCommandValidator()
+    {
+        RuleFor(c => c.Name).NotEmpty();
+        RuleFor(c => c.Name).MinimumLength(2);
+    }
+}
