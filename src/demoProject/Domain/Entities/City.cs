811408f7ae2765e56ddef02b1d67c575641bb910
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-09-28 23:20:13 +0400

    new country created and city refactored

--- src/demoProject/Domain/Entities/City.cs
+++ src/demoProject/Domain/Entities/City.cs
@@ -5,16 +5,27 @@ public class City:Entity<Guid>
 {
     public string? Name { get; set; }
 
+
     public ICollection<Company>? Companies { get; set; }
 
     public City()
     {
         Companies = new HashSet<Company>();
     }
+    public City(string? name):this()
+    {
+        Name = name;
+    }
     public City(Guid id, string name) : this()
     {
         Id = id;
         Name = name;
        
+    
+    }public City(Guid id, string name,DateTime createdDate) : this()
+    {
+        Id = id;
+        Name = name;
+        CreatedDate = createdDate;
     }
 }
---
+++ src/demoProject/Domain/Entities/Country.cs
@@ -0,0 +1,21 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+public  class Country:Entity<Guid>
+{
+    public string? Name { get; set; }
+
+    public ICollection<City>? Cities { get; set; }
+
+    public Country(Guid Id,string name)
+    {
+        Id = Id;
+        Name = name;
+    }
+
+    public Country()
+    {
+        Cities=new HashSet<City>();
+    }
+
+}
