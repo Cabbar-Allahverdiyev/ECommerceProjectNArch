0c37c34fbf2628c47bee7fde9e3a9cc42bdef1eb
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-10-16 23:48:21 +0400

    Discount tests were writed

---
+++ tests/Application.Tests/Features/Discounts/Commands/CreateDiscountTests.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Discounts.Commands.Create;
+using Application.Tests.Mocks.FakeData;
+using Application.Tests.Mocks.Repositories;
+using static Application.Features.Discounts.Commands.Create.CreateDiscountCommand;
+
+namespace Application.Tests.Features.Discounts.Commands;
+public class CreateDiscountTests:DiscountMockRepository
+{
+    private readonly CreateDiscountCommandValidator _validator;
+    private readonly CreateDiscountCommand _command;
+    private readonly CreateDiscountCommandHandler _handler;
+
+    public CreateDiscountTests(DiscountFakeData fakeData,
+                               CreateDiscountCommandValidator validator,
+                               CreateDiscountCommand command)
+        :base(fakeData)
+    {
+        _validator = validator;
+        _command = command;
+        _handler =new CreateDiscountCommandHandler(Mapper,MockRepository.Object,BusinessRules);
+    }
+}
---
+++ tests/Application.Tests/Mocks/Repositories/DiscountMockRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Features.Discounts.Profiles;
+using Application.Features.Discounts.Rules;
+using Application.Services.Repositories;
+using Application.Tests.Mocks.FakeData;
+using Core.Test.Application.Repositories;
+using Domain.Entities;
+using System;
+
+namespace Application.Tests.Mocks.Repositories;
+public class DiscountMockRepository : BaseMockRepository<IDiscountRepository, Discount, Guid, MappingProfiles, DiscountBusinessRules, DiscountFakeData>
+{
+    public DiscountMockRepository(DiscountFakeData fakeData) : base(fakeData)
+    {
+    }
+}
