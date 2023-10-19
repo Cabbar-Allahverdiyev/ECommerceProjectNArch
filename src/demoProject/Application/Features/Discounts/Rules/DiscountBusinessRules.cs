14bf104582bbfa5c7f55cc597abdb664d885e354
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-10-19 23:21:15 +0400

    discount business rules refactor

--- src/demoProject/Application/Features/Discounts/Rules/DiscountBusinessRules.cs
+++ src/demoProject/Application/Features/Discounts/Rules/DiscountBusinessRules.cs
@@ -21,6 +21,12 @@ public class DiscountBusinessRules : BaseBusinessRules
             throw new BusinessException(DiscountsBusinessMessages.DiscountNotExists);
         return Task.CompletedTask;
     }
+    public Task DiscountShouldNotExistWhenSelected(Discount? discount)
+    {
+        if (discount != null)
+            throw new BusinessException(DiscountsBusinessMessages.DiscountNameAlreadyExists);
+        return Task.CompletedTask;
+    }
 
     public async Task DiscountIdShouldExistWhenSelected(Guid id, CancellationToken cancellationToken)
     {
@@ -31,4 +37,38 @@ public class DiscountBusinessRules : BaseBusinessRules
         );
         await DiscountShouldExistWhenSelected(discount);
     }
+    public async Task DiscountIdShouldNotExistWhenSelected(Guid id, CancellationToken cancellationToken)
+    {
+        Discount? discount = await _discountRepository.GetAsync(
+            predicate: d => d.Id == id,
+            enableTracking: false,
+            cancellationToken: cancellationToken
+        );
+        await DiscountShouldNotExistWhenSelected(discount);
+    }
+
+    public async Task DiscountNameShouldNotExistWhenSelected(Discount? discount, CancellationToken cancellationToken)
+    {
+        if (discount == null)
+            throw new BusinessException(DiscountsBusinessMessages.DiscountIsNull);
+        Discount? result = await _discountRepository.GetAsync(
+        predicate: d => string.Equals(d.Name, discount.Name, StringComparison.OrdinalIgnoreCase),
+        enableTracking: false,
+        cancellationToken: cancellationToken);
+
+        await DiscountShouldNotExistWhenSelected(result);
+    }
+
+    public async Task DiscountNameShouldNotExistWhenUpdated(Discount? discount, string newName, CancellationToken cancellationToken)
+    {
+        if (discount == null)
+            throw new BusinessException(DiscountsBusinessMessages.DiscountIsNull);
+        Discount? result = await _discountRepository.GetAsync(
+        predicate: d => string.Equals(d.Name, newName, StringComparison.OrdinalIgnoreCase) && d.Id != discount.Id,
+        enableTracking: false,
+        cancellationToken: cancellationToken);
+        await DiscountShouldNotExistWhenSelected(result);
+    }
+
+    
 }
