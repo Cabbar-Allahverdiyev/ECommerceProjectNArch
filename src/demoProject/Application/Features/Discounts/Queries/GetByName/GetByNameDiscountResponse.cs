a690d5d7bad233a74c59399543d9b2acc1b73d2f
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-10-20 00:14:59 +0400

    get by name query writed

---
+++ src/demoProject/Application/Features/Discounts/Queries/GetByName/GetByNameDiscountResponse.cs
@@ -0,0 +1,10 @@
+﻿using Core.Application.Responses;
+
+namespace Application.Features.Discounts.Queries.GetByName;
+public class GetByNameDiscountResponse:IResponse
+{
+    public Guid Id { get; set; }
+    public decimal DiscountPercent { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ src/demoProject/Application/Features/Discounts/Queries/GetByName/GetByNameDiscountValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Discounts.Queries.GetByName;
+public class GetByNameDiscountValidator : AbstractValidator<GetByNameDiscountQuery>
+{
+    public GetByNameDiscountValidator()
+    {
+        RuleFor(d => d.Name).NotEmpty();
+    }
+}
