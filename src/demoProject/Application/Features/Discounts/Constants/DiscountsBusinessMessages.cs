9de800c72529e527dbbbac2b3e35dbd1a44e80c0
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-10-19 23:20:43 +0400

    discount business messages refactor

--- src/demoProject/Application/Features/Discounts/Constants/DiscountsBusinessMessages.cs
+++ src/demoProject/Application/Features/Discounts/Constants/DiscountsBusinessMessages.cs
@@ -3,4 +3,6 @@ namespace Application.Features.Discounts.Constants;
 public static class DiscountsBusinessMessages
 {
     public const string DiscountNotExists = "Discount not exists.";
+    public const string DiscountNameAlreadyExists = "Discount name already exists.";
+    public const string DiscountIsNull = "Discount is null.";
 }
