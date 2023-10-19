9527f803019b835d18f230e9a38e49e6c9c2b2bd
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-10-19 23:18:42 +0400

    validation error code refactor

--- src/corePackages/Core.Test/Application/Constants/ValidationErrorCodes.cs
+++ src/corePackages/Core.Test/Application/Constants/ValidationErrorCodes.cs
@@ -4,5 +4,7 @@ public static class ValidationErrorCodes
 {
     public static string NotEmptyValidator => "NotEmptyValidator";
     public static string MinimumLengthValidator => "MinimumLengthValidator";
+    public static string MaxLengthValidator => "MaximumLengthValidator";
     public static string EmailValidator => "EmailValidator";
+    public static string GreaterThanValidator => "GreaterThanValidator";
 }
