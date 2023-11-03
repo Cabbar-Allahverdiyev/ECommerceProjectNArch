85142776f361acc3c61f37fcc6d419f025279d53
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-11-03 23:41:31 +0400

    refactor

--- src/demoProject/Application/Features/Companies/Commands/Update/UpdateCompanyCommand.cs
+++ src/demoProject/Application/Features/Companies/Commands/Update/UpdateCompanyCommand.cs
@@ -18,7 +18,7 @@ public class UpdateCompanyCommand : IRequest<UpdatedCompanyResponse>, ISecuredRe
     public string? Name { get; set; }
     public string? AddressLine1 { get; set; }
     public string? AddressLine2 { get; set; }
-    public int CityId { get; set; }
+    public Guid CityId { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
 
--- src/demoProject/Application/Features/Companies/Commands/Update/UpdatedCompanyResponse.cs
+++ src/demoProject/Application/Features/Companies/Commands/Update/UpdatedCompanyResponse.cs
@@ -8,7 +8,7 @@ public class UpdatedCompanyResponse : IResponse
     public string? Name { get; set; }
     public string? AddressLine1 { get; set; }
     public string? AddressLine2 { get; set; }
-    public int CityId { get; set; }
+    public Guid CityId { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
 }
