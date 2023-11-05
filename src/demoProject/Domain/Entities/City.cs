0a3c82636accd7db1132a1f680bbe66574f54ff1
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-11-05 23:51:23 +0400

    refactor dto query

--- src/demoProject/Domain/Entities/City.cs
+++ src/demoProject/Domain/Entities/City.cs
@@ -1,33 +1,35 @@
 ﻿using Core.Persistence.Repositories;
 
 namespace Domain.Entities;
-public class City:Entity<Guid>
+public class City : Entity<Guid>
 {
-    public string? Name { get; set; }
-
+    public string Name { get; set; }
+    public Guid CountryId { get; set; }
 
     public ICollection<Company>? Companies { get; set; }
-    public ICollection<Country>? Countries { get; set; }
+    public Country? Country { get; set; }
 
     public City()
     {
         Companies = new HashSet<Company>();
-        Countries=new HashSet<Country>();
     }
-    public City(string? name):this()
+    public City(Guid countryId,string name) : this()
     {
         Name = name;
+        CountryId = countryId;
     }
-    public City(Guid id, string name) : this()
+    public City(Guid id, Guid countryId, string name) : this()
     {
         Id = id;
         Name = name;
-       
-    
-    }public City(Guid id, string name,DateTime createdDate) : this()
+        CountryId = countryId;
+
+    }
+    public City(Guid id, Guid countryId, string name, DateTime createdDate) : this()
     {
         Id = id;
         Name = name;
         CreatedDate = createdDate;
+        CountryId = countryId;
     }
 }
