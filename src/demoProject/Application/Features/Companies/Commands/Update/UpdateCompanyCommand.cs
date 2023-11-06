b50ad21b546604cb93b30033e0ce9fb1b66ed410
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-11-07 00:07:14 +0400

    refactor

--- src/demoProject/Application/Features/Companies/Commands/Update/UpdateCompanyCommand.cs
+++ src/demoProject/Application/Features/Companies/Commands/Update/UpdateCompanyCommand.cs
@@ -15,10 +15,10 @@ namespace Application.Features.Companies.Commands.Update;
 public class UpdateCompanyCommand : IRequest<UpdatedCompanyResponse>, ISecuredRequest, ICacheRemoverRequest, ILoggableRequest, ITransactionalRequest
 {
     public Guid Id { get; set; }
+    public Guid CityId { get; set; }
     public string? Name { get; set; }
     public string? AddressLine1 { get; set; }
     public string? AddressLine2 { get; set; }
-    public Guid CityId { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
 
