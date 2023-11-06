f6ab2ef05f291f16c28cec734b69508702a4374c
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-11-07 00:09:53 +0400

    refactor

--- src/demoProject/Application/Features/Countries/Profiles/MappingProfiles.cs
+++ src/demoProject/Application/Features/Countries/Profiles/MappingProfiles.cs
@@ -7,6 +7,8 @@ using AutoMapper;
 using Core.Application.Responses;
 using Domain.Entities;
 using Core.Persistence.Paging;
+using Application.Features.Countries.Dtos;
+using Application.Features.Countries.Queries.GetByName;
 
 namespace Application.Features.Countries.Profiles;
 
@@ -20,8 +22,12 @@ public class MappingProfiles : Profile
         CreateMap<Country, UpdatedCountryResponse>().ReverseMap();
         CreateMap<Country, DeleteCountryCommand>().ReverseMap();
         CreateMap<Country, DeletedCountryResponse>().ReverseMap();
-        CreateMap<Country, GetByIdCountryResponse>().ReverseMap();
-        CreateMap<Country, GetListCountryListItemDto>().ReverseMap();
+
+        CreateMap<City, GetCityInCountryDto>().ReverseMap();
+
+        CreateMap<Country, GetByIdCountryResponse>().ForMember(dest=>dest.Cities,act=>act.MapFrom(c=>c.Cities)).ReverseMap();
+        CreateMap<Country, GetByNameCountryResponse>().ForMember(dest=>dest.Cities,act=>act.MapFrom(c=>c.Cities)).ReverseMap();
+        CreateMap<Country, GetListCountryListItemDto>().ForMember(dest => dest.Cities, act => act.MapFrom(c => c.Cities)).ReverseMap();
         CreateMap<IPaginate<Country>, GetListResponse<GetListCountryListItemDto>>().ReverseMap();
     }
 }
