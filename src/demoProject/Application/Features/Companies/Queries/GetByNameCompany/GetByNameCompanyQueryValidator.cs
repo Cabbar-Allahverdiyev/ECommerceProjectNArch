f39c79a4afd58e234494d689db0d904a64fd510c
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-11-14 23:02:43 +0400

    refactor

--- src/demoProject/Application/Features/Companies/Queries/GetByNameCompany/GetByNameCompanyQueryValidator.cs
+++
@@ -1,8 +0,0 @@
-using FluentValidation;
-
-namespace Application.Features.Companies.Queries.GetByNameCompany;
-
-public class GetByNameCompanyQueryValidator : AbstractValidator<GetByNameCompanyQuery>
-{
-    public GetByNameCompanyQueryValidator() { RuleFor(c => c.Name).NotEmpty(); }
-}
