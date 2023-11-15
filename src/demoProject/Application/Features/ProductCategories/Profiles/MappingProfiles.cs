fd0e178f68f331c412fb760ca45fdef2ed3d2972
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-11-15 23:15:36 +0400

    refactor for two added dtos

--- src/demoProject/Application/Features/ProductCategories/Profiles/MappingProfiles.cs
+++ src/demoProject/Application/Features/ProductCategories/Profiles/MappingProfiles.cs
@@ -8,6 +8,7 @@ using Core.Application.Responses;
 using Domain.Entities;
 using Core.Persistence.Paging;
 using Application.Features.ProductCategories.Queries.GetByName;
+using Application.Features.ProductCategories.Dtos;
 
 namespace Application.Features.ProductCategories.Profiles;
 
@@ -21,9 +22,22 @@ public class MappingProfiles : Profile
         CreateMap<ProductCategory, UpdatedProductCategoryResponse>().ReverseMap();
         CreateMap<ProductCategory, DeleteProductCategoryCommand>().ReverseMap();
         CreateMap<ProductCategory, DeletedProductCategoryResponse>().ReverseMap();
-        CreateMap<ProductCategory, GetByIdProductCategoryResponse>().ReverseMap();
-        CreateMap<ProductCategory, GetListProductCategoryListItemDto>().ReverseMap();
-        CreateMap<ProductCategory, GetByNameProductCategoryResponse>().ForMember(dest => dest.Parent, opt => opt.MapFrom(src => src.Parent));
+
+        CreateMap<ProductCategory, GetParentCategoryInProductCategoryDto>().ReverseMap();  
+        CreateMap<Product, GetProductsInProductCategoryDto>().ReverseMap();  
+
+        CreateMap<ProductCategory, GetByIdProductCategoryResponse>()
+            .ForMember(dest=>dest.Parent,act=>act.MapFrom(src=>src.Parent))
+            .ForMember(dest=>dest.Products,act=>act.MapFrom(src=>src.Products))
+            .ReverseMap();
+        CreateMap<ProductCategory, GetByNameProductCategoryResponse>()
+            .ForMember(dest => dest.Parent, act => act.MapFrom(src => src.Parent))
+            .ForMember(dest => dest.Products, act => act.MapFrom(src => src.Products))
+            .ReverseMap();
+        CreateMap<ProductCategory, GetListProductCategoryListItemDto>()
+            .ForMember(dest => dest.Parent, act => act.MapFrom(src => src.Parent))
+            .ForMember(dest => dest.Products, act => act.MapFrom(src => src.Products))
+            .ReverseMap();
         CreateMap<IPaginate<ProductCategory>, GetListResponse<GetListProductCategoryListItemDto>>().ReverseMap();
     }
 }
