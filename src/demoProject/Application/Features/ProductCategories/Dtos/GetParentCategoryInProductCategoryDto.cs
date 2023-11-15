f43ed18ddd48c1172f53e81390f31351b4a57101
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-11-15 23:14:39 +0400

    getParentInCategoryDto writed

---
+++ src/demoProject/Application/Features/ProductCategories/Dtos/GetParentCategoryInProductCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿using Core.Application.Dtos;
+
+namespace Application.Features.ProductCategories.Dtos;
+public class GetParentCategoryInProductCategoryDto : IDto
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
