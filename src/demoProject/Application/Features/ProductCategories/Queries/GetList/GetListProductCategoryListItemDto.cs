b6525af3f8a6c95797b4447485b166358f5b0a65
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-11-15 23:16:26 +0400

    refactor

--- src/demoProject/Application/Features/ProductCategories/Queries/GetList/GetListProductCategoryListItemDto.cs
+++ src/demoProject/Application/Features/ProductCategories/Queries/GetList/GetListProductCategoryListItemDto.cs
@@ -1,3 +1,4 @@
+using Application.Features.ProductCategories.Dtos;
 using Core.Application.Dtos;
 
 namespace Application.Features.ProductCategories.Queries.GetList;
@@ -6,6 +7,7 @@ public class GetListProductCategoryListItemDto : IDto
 {
     public Guid Id { get; set; }
     public string? Name { get; set; }
-    public Guid? ParentId { get; set; }
     public string? Description { get; set; }
+    public GetParentCategoryInProductCategoryDto? Parent { get; set; }
+    public ICollection<GetProductsInProductCategoryDto>? Products { get; set; }
 }
