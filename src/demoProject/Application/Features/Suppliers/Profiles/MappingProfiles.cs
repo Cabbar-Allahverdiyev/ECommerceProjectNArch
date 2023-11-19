ed5faa2ae5b689876879b3607b99565879157b54
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-11-19 22:25:27 +0400

    Profiles were refactored for some QueryDtos

--- src/demoProject/Application/Features/Suppliers/Profiles/MappingProfiles.cs
+++ src/demoProject/Application/Features/Suppliers/Profiles/MappingProfiles.cs
@@ -7,6 +7,10 @@ using AutoMapper;
 using Core.Application.Responses;
 using Domain.Entities;
 using Core.Persistence.Paging;
+using Core.Security.Entities;
+using Application.Features.Suppliers.Dtos;
+using Application.Features.Suppliers.Queries.GetByUserId;
+using Application.Features.Suppliers.Queries.GetByCompanyId;
 
 namespace Application.Features.Suppliers.Profiles;
 
@@ -20,8 +24,31 @@ public class MappingProfiles : Profile
         CreateMap<Supplier, UpdatedSupplierResponse>().ReverseMap();
         CreateMap<Supplier, DeleteSupplierCommand>().ReverseMap();
         CreateMap<Supplier, DeletedSupplierResponse>().ReverseMap();
-        CreateMap<Supplier, GetByIdSupplierResponse>().ReverseMap();
-        CreateMap<Supplier, GetListSupplierListItemDto>().ReverseMap();
+
+        CreateMap<User, GetUserInSupplierQueryDto>().ReverseMap();
+        CreateMap<Company, GetCompanyInSupplierQueryDto>().ReverseMap();
+        CreateMap<Product,GetProductsInSupplierQueryDto>().ReverseMap();
+
+        CreateMap<Supplier, GetByIdSupplierResponse>()
+            .ForMember(dest=>dest.User,act=>act.MapFrom(src=>src.User))
+            .ForMember(dest=>dest.Company,act=>act.MapFrom(src=>src.Company))
+            .ForMember(dest=>dest.Products,act=>act.MapFrom(src=>src.Products)).ReverseMap();
+
+        CreateMap<Supplier, GetByUserIdSupplierResponse>()
+           .ForMember(dest => dest.User, act => act.MapFrom(src => src.User))
+           .ForMember(dest => dest.Company, act => act.MapFrom(src => src.Company))
+           .ForMember(dest => dest.Products, act => act.MapFrom(src => src.Products)).ReverseMap();
+
+        CreateMap<Supplier, GetByCompanyIdSupplierResponse>()
+           .ForMember(dest => dest.User, act => act.MapFrom(src => src.User))
+           .ForMember(dest => dest.Company, act => act.MapFrom(src => src.Company))
+           .ForMember(dest => dest.Products, act => act.MapFrom(src => src.Products)).ReverseMap();
+
+        CreateMap<Supplier, GetListSupplierListItemDto>()
+            .ForMember(dest => dest.User, act => act.MapFrom(src => src.User))
+            .ForMember(dest => dest.Company, act => act.MapFrom(src => src.Company))
+            .ForMember(dest => dest.Products, act => act.MapFrom(src => src.Products)).ReverseMap();
+
         CreateMap<IPaginate<Supplier>, GetListResponse<GetListSupplierListItemDto>>().ReverseMap();
     }
 }
