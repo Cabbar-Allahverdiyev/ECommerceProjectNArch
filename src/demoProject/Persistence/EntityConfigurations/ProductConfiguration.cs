1faf2a5c3341a8cdcc878e84e7811a40eaa372d6
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2023-11-20 21:23:29 +0400

    entity configs was refactored for Product

--- src/demoProject/Persistence/EntityConfigurations/ProductConfiguration.cs
+++ src/demoProject/Persistence/EntityConfigurations/ProductConfiguration.cs
@@ -11,20 +11,20 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
         builder.ToTable("Products").HasKey(p => p.Id);
 
         builder.Property(p => p.Id).HasColumnName("Id").IsRequired();
-        builder.Property(p => p.CategoryId).HasColumnName("CategoryId");
-        builder.Property(p => p.BrandId).HasColumnName("BrandId");
-        builder.Property(p => p.SupplierId).HasColumnName("SupplierId");
-        builder.Property(p => p.DiscountId).HasColumnName("DiscountId");
-        builder.Property(p => p.ProductInventorId).HasColumnName("ProductInventorId");//InventorId edib yoxla
+        builder.Property(p => p.CategoryId).HasColumnName("CategoryId").IsRequired();
+        builder.Property(p => p.BrandId).HasColumnName("BrandId").IsRequired();
+        builder.Property(p => p.SupplierId).HasColumnName("SupplierId").IsRequired();
+        builder.Property(p => p.DiscountId).HasColumnName("DiscountId").IsRequired();
+        builder.Property(p => p.ProductInventorId).HasColumnName("ProductInventorId").IsRequired();//InventorId edib yoxla
         builder.Property(p => p.UnitsOnOrder).HasColumnName("UnitsOnOrder");
         builder.Property(p => p.ReorderLevel).HasColumnName("ReorderLevel");
-        builder.Property(p => p.PurchasePrice).HasColumnName("PurchasePrice");
-        builder.Property(p => p.UnitPrice).HasColumnName("UnitPrice");
-        builder.Property(p => p.Name).HasColumnName("Name");
+        builder.Property(p => p.PurchasePrice).HasColumnName("PurchasePrice").IsRequired();
+        builder.Property(p => p.UnitPrice).HasColumnName("UnitPrice").IsRequired();
+        builder.Property(p => p.Name).HasColumnName("Name").IsRequired();
         builder.Property(p => p.QuantityPerUnit).HasColumnName("QuantityPerUnit");
-        builder.Property(p => p.SKU).HasColumnName("SKU");
+        builder.Property(p => p.SKU).HasColumnName("SKU").IsRequired();
         builder.Property(p => p.Description).HasColumnName("Description");
-        builder.Property(p => p.IsDiscontinued).HasColumnName("IsDiscontinued");
+        builder.Property(p => p.IsDiscontinued).HasColumnName("IsDiscontinued").IsRequired();
         builder.Property(p => p.CreatedDate).HasColumnName("CreatedDate").IsRequired();
         builder.Property(p => p.UpdatedDate).HasColumnName("UpdatedDate");
         builder.Property(p => p.DeletedDate).HasColumnName("DeletedDate");
@@ -33,7 +33,6 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
         builder.HasOne(c => c.Category);
         builder.HasOne(c => c.Supplier);
         builder.HasOne(c => c.Discount);
-        //builder.HasOne(c => c.ProductInventor);
         builder.HasOne(c => c.ProductInventor).WithOne(i => i.Product).HasForeignKey<ProductInventor>(i => i.Id);
 
         builder.HasQueryFilter(p => !p.DeletedDate.HasValue);
