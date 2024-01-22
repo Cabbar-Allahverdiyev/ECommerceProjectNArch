c73440f575113446f514939fc7a4ba37ba31f51a
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2024-01-22 23:08:02 +0400

    refactored

--- src/demoProject/Application/Common/Helpers/ProductHelpers/SkuHelper.cs
+++ src/demoProject/Application/Common/Helpers/ProductHelpers/SkuHelper.cs
@@ -4,7 +4,12 @@ namespace Application.Common.Helpers.ProductHelpers;
 public static class SkuHelper
 {
     public async static Task<string> GenerateSKU(this Product product)
-    {//name ler ile yaz bu metodu include edende gelir
+    {
+        //SKU cox uzundur biraz qisalt meselen: "bbcad47a-6373-4904-a26c-51f1674d56ec9fefc339-0
+        //ee7-441a-b68d-a025931cb2d9e9077bf5-3b56-41a5-bfcf-7b3fe097bf1cb08dd13b-8144-4198-a69c
+        //-f1b435284d66a7a28416-3440-4dcf-b881-5b3b946a30cc-STR-005,00",
+
+        //name ler ile yaz bu metodu include edende gelir
         //int categoryId= await product.CategoryId.ToInt();
         //int brandId= await product.BrandId.ToInt();
         //int supplierId= await product.SupplierId.ToInt();
