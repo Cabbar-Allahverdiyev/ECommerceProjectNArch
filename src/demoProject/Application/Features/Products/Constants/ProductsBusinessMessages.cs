01925639a728b32e9f2cd9666eed9fa5bd9ef789
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2024-01-22 23:08:13 +0400

    refactored

--- src/demoProject/Application/Features/Products/Constants/ProductsBusinessMessages.cs
+++ src/demoProject/Application/Features/Products/Constants/ProductsBusinessMessages.cs
@@ -5,4 +5,6 @@ public static class ProductsBusinessMessages
     public const string ProductNotExists = "Product not exists.";
 
     public const string PurchasePriceShouldBeLessThanOrEqualUnitPrice = "Purchase price should be less than or equal unit price ";
+
+    public const  string ProductExists = "Product exists.";
 }
--- src/demoProject/Application/Features/Products/Rules/ProductBusinessRules.cs
+++ src/demoProject/Application/Features/Products/Rules/ProductBusinessRules.cs
@@ -3,6 +3,8 @@ using Application.Services.Repositories;
 using Core.Application.Rules;
 using Core.CrossCuttingConcerns.Exceptions.Types;
 using Domain.Entities;
+using Nest;
+using System.Threading;
 
 namespace Application.Features.Products.Rules;
 
@@ -21,6 +23,12 @@ public class ProductBusinessRules : BaseBusinessRules
             throw new BusinessException(ProductsBusinessMessages.ProductNotExists);
         return Task.CompletedTask;
     }
+    public Task ProductShouldNotExistWhenSelected(Product? product)
+    {
+        if (product != null)
+            throw new BusinessException(ProductsBusinessMessages.ProductExists);
+        return Task.CompletedTask;
+    }
 
     public async Task ProductIdShouldExistWhenSelected(Guid id, CancellationToken cancellationToken)
     {
@@ -32,14 +40,33 @@ public class ProductBusinessRules : BaseBusinessRules
         await ProductShouldExistWhenSelected(product);
     }
 
-    public   Task ProductPurchasePriceShouldBeLessThanUnitPrice(decimal purchasePrice, decimal unitPrice)
+    public   Task ProductPurchasePriceShouldBeLessThanUnitPrice(decimal? purchasePrice, decimal? unitPrice)
     {
         if (purchasePrice >= unitPrice) throw new BusinessException(ProductsBusinessMessages.PurchasePriceShouldBeLessThanOrEqualUnitPrice);
         return Task.CompletedTask;
     }
 
-    public Task ProductNameShouldNotHasSupplierAndColorUsedAlreadyWhenInsert(string? name, Guid supplierId, Guid productColorId, CancellationToken cancellationToken)
+    public async Task ProductNameShouldNotHasSupplierAndColorUsedAlreadyWhenInsert(string? name, Guid supplierId, Guid productColorId, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        Product? product = await _productRepository.GetAsync(
+            predicate: p=>p.ProductColorId==productColorId && p.SupplierId==supplierId && p.Name == name,
+            enableTracking :false,
+            cancellationToken: cancellationToken);
+        await ProductShouldNotExistWhenSelected(product);
     }
+    public async Task ProductNameShouldNotHasSupplierAndColorUsedAlreadyWhenUpdate(Guid productId, 
+        string? name, Guid supplierId, Guid productColorId, CancellationToken cancellationToken)
+    {
+        Product? product = await _productRepository.GetAsync(
+            predicate: p => p.ProductColorId == productColorId && p.SupplierId == supplierId && p.Name == name && p.Id != productId,
+        enableTracking: false,
+            cancellationToken: cancellationToken);
+        await ProductShouldNotExistWhenSelected(product);
+    }
+
+    public async Task ProductPurchasePriceShouldBeLessThanUnitPriceWhenUpdated(decimal? purchasePrice, decimal? unitPrice)
+    {
+        if (purchasePrice > 0 && unitPrice > 0) await ProductPurchasePriceShouldBeLessThanUnitPrice(purchasePrice, unitPrice);
+    }
+
 }
