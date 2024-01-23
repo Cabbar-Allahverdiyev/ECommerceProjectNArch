e3f1c97209859a73141ae3769788faf4b1f58d00
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2024-01-23 23:48:48 +0400

    Product query  dtos created

---
+++ src/demoProject/Application/Features/Products/Dtos/GetSupplierInProductQueryDto.cs
@@ -0,0 +1,10 @@
+﻿using Core.Application.Dtos;
+
+namespace Application.Features.Products.Dtos;
+public class GetSupplierInProductQueryDto:IDto
+{
+    public Guid Id { get; set; }
+    public Guid CompanyId { get; set; }
+    public int UserId { get; set; }
+    public string? Description { get; set; }
+}
