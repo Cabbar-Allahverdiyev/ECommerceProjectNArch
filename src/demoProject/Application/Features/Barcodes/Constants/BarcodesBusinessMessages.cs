40f5266876dac484172fcfe5c82eca4d4073cc48
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2024-07-23 00:19:21 +0400

    business rules refactored

--- src/demoProject/Application/Features/Barcodes/Constants/BarcodesBusinessMessages.cs
+++ src/demoProject/Application/Features/Barcodes/Constants/BarcodesBusinessMessages.cs
@@ -3,4 +3,8 @@ namespace Application.Features.Barcodes.Constants;
 public static class BarcodesBusinessMessages
 {
     public const string BarcodeNotExists = "Barcode not exists.";
+    public const string BarcodeNumberMustContainOnlyDigits = "Barcode number must contain only digits.";
+    public const string BarcodeExists = "Barcode exists.";
+    public const string BarcodeNumberAlreadyExists = "Barcode number already exists.";
+    public const string BarcodeNumberNotInCorrectFormat = "Barcode number not correct format. Fix the checksum. Checksum is the last digit of the barcode number";
 }
--- src/demoProject/Application/Features/Barcodes/Rules/BarcodeBusinessRules.cs
+++ src/demoProject/Application/Features/Barcodes/Rules/BarcodeBusinessRules.cs
@@ -1,3 +1,4 @@
+using Application.Common.Helpers.BarcodeHelpers;
 using Application.Features.Barcodes.Constants;
 using Application.Services.Repositories;
 using Core.Application.Rules;
@@ -9,16 +10,25 @@ namespace Application.Features.Barcodes.Rules;
 public class BarcodeBusinessRules : BaseBusinessRules
 {
     private readonly IBarcodeRepository _barcodeRepository;
+    private readonly IBarcodeHelper _barcodeHelper;
 
-    public BarcodeBusinessRules(IBarcodeRepository barcodeRepository)
+    public BarcodeBusinessRules(IBarcodeRepository barcodeRepository, IBarcodeHelper barcodeHelper)
     {
         _barcodeRepository = barcodeRepository;
+        _barcodeHelper = barcodeHelper;
     }
 
     public Task BarcodeShouldExistWhenSelected(Barcode? barcode)
     {
         if (barcode == null)
             throw new BusinessException(BarcodesBusinessMessages.BarcodeNotExists);
+        return Task.CompletedTask;
+    }
+
+    public Task BarcodeShouldNotExistWhenSelected(Barcode? barcode,string? message= BarcodesBusinessMessages.BarcodeExists)
+    {
+        if (barcode != null)
+            throw new BusinessException(message);
         return Task.CompletedTask;
     }
 
@@ -31,4 +41,24 @@ public class BarcodeBusinessRules : BaseBusinessRules
         );
         await BarcodeShouldExistWhenSelected(barcode);
     }
+
+    public async Task BarcodeNumberShouldExistWhenSelected(string? barcodeNumber, CancellationToken cancellationToken)
+    {
+        Barcode? barcode = await _barcodeRepository.GetAsync(
+            predicate: b => b.BarcodeNumber == barcodeNumber,
+            enableTracking: false,
+            cancellationToken: cancellationToken
+        );
+
+        await BarcodeShouldNotExistWhenSelected(barcode,BarcodesBusinessMessages.BarcodeNumberAlreadyExists); 
+    }
+
+    public Task ChekSumMustCorrect(string ? chekSum)
+    {
+        if(_barcodeHelper.CheckChecksum(chekSum)) 
+            throw new BusinessException(BarcodesBusinessMessages.BarcodeNumberNotInCorrectFormat);
+        return Task.CompletedTask;
+    }
+
+    
 }
