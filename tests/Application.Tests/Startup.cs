bf7e2743900414a02a5149ae41311df32cab7d21
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2025-01-14 19:29:47 +0400

    startup refactored

--- tests/Application.Tests/Startup.cs
+++ tests/Application.Tests/Startup.cs
@@ -8,12 +8,18 @@ public sealed class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddUsersServices();
+        services.AddBarcodeServices();
         services.AddCityServices();
+        services.AddCompanyServices();
         services.AddCountryServices();
         services.AddDiscountServices();
+        services.AddProductServices();
         services.AddProductBrandServices();
         services.AddProductCategoryServices();
+        services.AddProductColorServices();
         services.AddProductInventorServices();
+        services.AddSellerServices();
+        services.AddShopServices();
         services.AddSupplierServices();
     }
 }
